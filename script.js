24a3457a459732f42325c3f896df65431f17aaee
LirPes <lironpes@@gmail.com>
2025-12-11 00:01:11 +0200

    create the buttons dynamically

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
+// create buttons
+const screen = document.querySelector("#display");
+const container = document.querySelector(".buttons");
+
+//operators
+const operatorsRow = document.createElement("div");
+operatorsRow.classList.add('row','operations')
+const operators = ["+","-","*","/"];
+const operatorsNames = ['add','substract','multiply','divide']
+
+for(let i = 0; i<operators.length; i++) {
+    const button = document.createElement('button')
+    button.innerText = `${operators[i]}`
+    button.id = `${operatorsNames[i]}`
+    button.addEventListener('click', pressButton)
+    operatorsRow.appendChild(button)
+}
+
+container.insertBefore(operatorsRow, container.firstChild)
+
+//numbers
+const numbers = [[7,8,9],[4,5,6],[1,2,3]]
+numbers.forEach((row) => {
+    let rowNumbers = document.createElement('div');
+    rowNumbers.classList.add("row")
+    
+    row.forEach((num) => {
+        const button = document.createElement('button')
+        button.innerText = `${num}`
+        button.id = `number-${num}`
+        button.addEventListener('click', pressButton)
+        rowNumbers.appendChild(button)
+    })
+
+    container.insertBefore(rowNumbers, container.firstChild)
+})
 
 exrcise = "";
 
 function pressButton(event) {
-    const screen = document.querySelector(".display");
     const num = event.target.innerHTML;
     screen.textContent += num;
     exrcise += num;
 }
 
 function calculate() {
-    const operators = ["+","-","/","*"];
     const operator = operators.find(op => exrcise.indexOf(op) > -1);
     const firstNum = parseInt(exrcise.substring(0,exrcise.indexOf(operator)));
     const secondNum = parseInt(exrcise.substring(exrcise.indexOf(operator) + 1));
@@ -30,14 +64,11 @@ function calculate() {
             break;
     }
 
-    const screen = document.querySelector(".display");
     screen.innerHTML = result;
     exrcise = ""
 }
 
 function clearScreen() {
-    const screen = document.querySelector(".display");
     screen.innerHTML = "&nbsp;";
     exrcise = "";
 }
-
